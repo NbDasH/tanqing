766570a912fb6c71a582a1b9c4bfe8c304781b3d
Azhun <157501134@qq.com>
2013-12-10 20:07:13 +0800

    Banner设置不缓存

--- template/banner.php
+++ template/banner.php
@@ -5,9 +5,9 @@
             	$b_db = new db;
 				$banners = $b_db->select('banners');
 			?>
-            <a href="http://<?php echo $banners[0]['link']; ?>"><img src="img/b1.jpg?rand=<?php echo rand(); ?>" style="z-index:1"></a>
-            <a href="http://<?php echo $banners[1]['link']; ?>"><img src="img/b2.jpg?rand=<?php echo rand(); ?>"></a>
-            <a href="http://<?php echo $banners[2]['link']; ?>"><img src="img/b3.jpg?rand=<?php echo rand(); ?>"></a>
+            <a href="http://<?php echo $banners[0]['link']; ?>"><img src="img/b1.jpg" style="z-index:1"></a>
+            <a href="http://<?php echo $banners[1]['link']; ?>"><img src="img/b2.jpg"></a>
+            <a href="http://<?php echo $banners[2]['link']; ?>"><img src="img/b3.jpg"></a>
         </div>
         <ul class="btns">
         	<li class="hover">1</li>
